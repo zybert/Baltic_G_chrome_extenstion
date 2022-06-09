2823ddcf2d5e8bfddcbc3047441a7a9a57332f67
Mikołaj Sykucki <59975611+zybert@users.noreply.github.com>
2022-06-10 00:20:49 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+
+
+let age=27
+
+
+
+//Convert age to birth year. Current year - age - 1day. 
+let today = new Date();
+birthYear = String(today.getFullYear() - age);
+birthMonth = String(today.getMonth() + 1).padStart(2, '0');
+birthDay = String(today.getDate()).padStart(2, '0');
+
+let birthData = (birthYear + birthMonth + birthDay);
+    
+
+//Adding a gender number.
+let century = birthData.substring(0, 2);
+let gender = 1;
+genderNumber = century * 2 -34 - gender;
+console.log('Century: ' + century);
+genderNumber = String(genderNumber);
+console.log(genderNumber);
+
+personalCode = genderNumber.substring(0) + birthData.substring(2,10);
+
+
+console.log(personalCode);
+
+let serialNumber = String(Math.floor(Math.random() * 999));
+serialNumber = serialNumber.padStart(3, '0');
+
+console.log(serialNumber);
+
+personalCode = personalCode +serialNumber;
+console.log('Personal code: ' + personalCode);
+
+//Check sum number. - from wikipedia
+
+function lt_nin_checksum(code) {
+    var b = 1, c = 3, d = 0, e = 0, i, digit;
+    for (i = 0; i < 10; i++) {
+      digit = parseInt(code[i]);
+      d += digit * b;
+      e += digit * c;
+      b++; if (b == 10) b = 1;
+      c++; if (c == 10) c = 1;
+    }
+    d = d % 11;
+    e = e % 11;
+    if (d < 10)
+      return d;
+    else if (e < 10)
+      return e;
+    else
+      return 0;
+  }
+  
+personalCode = personalCode + lt_nin_checksum(personalCode); //adding 11 checksum digit.
+
+console.log(personalCode);
+document.write(personalCode)
